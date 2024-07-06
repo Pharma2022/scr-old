646f8969ba41bce602ce8bdafec33d23bf599009
Shah <111680056+Pharma2022@users.noreply.github.com>
2024-07-06 15:10:32 +0100

    Update data.js
    added oohstocklist and non-form form

--- src/data.js
+++ src/data.js
@@ -7,9 +7,9 @@ const Links=[
 {href:"https://servicefinder.nhs.uk/login", title:'Service Finder',active:false},
 {href:"https://connect.careflowapp.com/", title:'Careflow Connect',active:false},
 {href:"https://rap-vitals.syhapp.com/vpClinical/Login#/", title:'Vitals',active:false},
-{href:`https://pharmoutcomes.org/pharmoutcomes/`, title:'DMS',active:false}
-,
-
+{href:`https://pharmoutcomes.org/pharmoutcomes/`, title:'DMS',active:false},
+{href:`https://nhs.sharepoint.com/sites/msteams_e9b1b6/Lists/Nonformulary%20form`, title:'Non-formulary form',active:false}
+{href:`https://oohstocklist.netlify.app`, title:'All Wards Stocklist',active:false}
 ]
 
 const navLinks=Links.map(item=>({...item,id:nanoid()}))
