194978724bbd6c8ee3c71777602b322aceacff43
Shah <111680056+Pharma2022@users.noreply.github.com>
2024-07-06 19:21:55 +0100

    Update data.js
    removed vitals link

--- src/data.js
+++ src/data.js
@@ -6,7 +6,6 @@ const Links=[
 {href:"https://epma.nmh.nhs.uk/", title:'EPMA',active:false},
 {href:"https://servicefinder.nhs.uk/login", title:'Service Finder',active:false},
 {href:"https://connect.careflowapp.com/", title:'Careflow Connect',active:false},
-{href:"https://rap-vitals.syhapp.com/vpClinical/Login#/", title:'Vitals',active:false},
 {href:`https://pharmoutcomes.org/pharmoutcomes/`, title:'DMS',active:false},
 {href:`https://nhs.sharepoint.com/sites/msteams_e9b1b6/Lists/Nonformulary%20form`, title:'Non-formulary form',active:false},
 {href:`https://oohstocklist.netlify.app`, title:'All Wards Stocklist',active:false}
