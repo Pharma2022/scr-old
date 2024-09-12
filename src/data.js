814389d45e84f0482100b1d1540bcbfef694e814
Shah <111680056+Pharma2022@users.noreply.github.com>
2024-09-12 09:49:59 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -8,7 +8,8 @@ const Links=[
 {href:"https://connect.careflowapp.com/", title:'Careflow Connect',active:false},
 {href:`https://pharmoutcomes.org/pharmoutcomes/`, title:'DMS',active:false},
 {href:`https://nhs.sharepoint.com/sites/msteams_e9b1b6/Lists/Nonformulary%20form`, title:'Non-formulary form',active:false},
-{href:`https://oohstocklist.netlify.app`, title:'All Wards Stocklist',active:false}
+{href:`https://oohstocklist.netlify.app`, title:'All Wards Stocklist',active:false},
+{href:`https://nhs.sharepoint.com/sites/msteams_e9b1b6/Lists/POD%20savings/AllItems.aspx`, title:'Pod Savings',active:false},
 ]
 
 const navLinks=Links.map(item=>({...item,id:nanoid()}))
